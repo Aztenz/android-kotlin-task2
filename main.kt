e254524521f00bb6a8dfc55bc4ccd0642fff846f
Omar Muhammad <aztenz@outlook.com>
2022-08-12 10:39:37 +0200

    [add] function elementInStrings(scanner, int)

--- main.kt
+++ main.kt
@@ -1,2 +1,21 @@
-class main {
+import java.util.*
+
+fun elementInStrings(scan: Scanner, n: Int) {
+    val strings: MutableList<String> = mutableListOf()
+    for (i in 0..n) {
+        strings.add(i, scan.nextLine())
+    }
+    for (i in 0..n) {
+        if (strings[i].contains("B", true) or strings[i].contains("M", true))
+            strings[i] = strings[i]+ " Changed to => "+ strings[i].uppercase()
+    }
+    for (i in 0..n)
+        println(strings[i])
+}
+fun main() {
+    println("Enter array size: ")
+    val scan = Scanner(System.`in`)
+    val n = scan.nextInt()
+    println("Enter Array elements: ")
+    elementInStrings(scan, n)
 }
